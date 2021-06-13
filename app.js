123100ecb6b1ec52c3e9bc48d8f4a0de99d6fe7c
sandeep8080 <sandeepgarg14319@gmail.com>
2021-06-13 09:27:25 +0530

    Some changes

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 var app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 const port = process.env.PORT || 5000;
 
 app.use('/', indexRouter);
